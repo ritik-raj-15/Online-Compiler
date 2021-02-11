02822a7a24b70ad9dac53004fa678facff4d2a4f
Ritik <ritik1384.cse18@chitkara.edu.in>
2021-02-11 17:21:13 +0530

    Phase 2: API Implemented

---
+++ script.js
@@ -0,0 +1,69 @@
+var textarea=document.getElementById("textarea");
+var index=document.getElementById("index");
+var count=2;
+textarea.addEventListener("keyup", (event)=>{
+ if(event.keyCode === 13)
+    {
+        index.innerHTML+=`<li>${count}</li>`;
+        count++;
+    }
+});
+
+ textarea.addEventListener("keyup", (event)=>{
+    if(event.key === "Backspace")
+    {
+        index.removeChild(index.lastChild);
+        count--;
+    }
+});
+var compile = document.getElementById("compile");
+compile.addEventListener("click",()=>{
+    const code=textarea.value;
+    const id=document.getElementById("languages").value;
+   // console.log(id,code);
+    //post request to server 
+    var request = new XMLHttpRequest();
+    request.open("POST","https://codequotient.com/api/executeCode");
+    request.setRequestHeader("Content-Type","application/json");
+    request.send(JSON.stringify({"code":code,langId:id}));
+    request.onreadystatechange = function(){
+        if(request.readyState == 4)
+        {
+        const codeId = JSON.parse(request.responseText).codeId;
+        console.log(codeId);
+        //get request to server
+        var result=setTimeout(()=>{
+        var getrequest = new XMLHttpRequest();
+         getrequest.open("GET","https://codequotient.com/api/codeResult/"+codeId);
+         getrequest.send();
+         getrequest.onreadystatechange=function(){
+         if(getrequest.readyState==4)
+         {
+            var data = JSON.parse(JSON.parse(getrequest.responseText).data);   
+                console.log(data);
+               let output=data.output;
+                if(output!=='')
+                {
+                    document.getElementById("output").innerHTML=output;
+                }
+                else{
+                    let error = data.errors;
+                    document.getElementById("output").innerHTML=error;
+                }
+         }
+          }
+
+        },3000);
+        }
+    }
+});
+//CAUTION: THIS API IS PRIVATE AND WORKS ONLY ON CODEQUOTIENT SITE;
+//this jquery script help the textarea and div to scroll synchronously!
+$("#wid").scroll(function () { 
+    $("#textarea").scrollTop($("#wid").scrollTop());
+    $("#textarea").scrollLeft($("#wid").scrollLeft());
+  });
+  $("#textarea").scroll(function () { 
+    $("#wid").scrollTop($("#textarea").scrollTop());
+    $("#wid").scrollLeft($("#textarea").scrollLeft());
+  });
